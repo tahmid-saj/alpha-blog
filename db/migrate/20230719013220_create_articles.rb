876ac5958cbcbf507a4dd8e6831154e39c4da7b1
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-18 21:47:54 -0400

    Adding articles migrations

--- db/migrate/20230719013220_create_articles.rb
+++ db/migrate/20230719013220_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-
-      t.timestamps
+      t.string :title
+      t.text :description
     end
   end
 end
