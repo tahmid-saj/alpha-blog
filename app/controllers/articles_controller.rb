83fe8468ff2342ddac394e3e051bf5f1fb04b9dc
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-20 20:50:53 -0400

    Validating article inputs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
   end
 end
