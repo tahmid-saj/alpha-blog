528c91835d479c6e474956fc0a1d0b93e458858c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-08 02:36:12 -0400

    Bootstrap failure

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,8 @@
 import { Application } from "@hotwired/stimulus"
 
+import "bootstrap"
+import "../stylesheets/application"
+
 const application = Application.start()
 
 // Configure Stimulus development experience
