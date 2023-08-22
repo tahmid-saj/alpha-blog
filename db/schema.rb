fd4684db4a805b47e469557aa570fd9299e5bdeb
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-22 01:54:06 -0400

    1-1 associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_014528) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_053718) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
